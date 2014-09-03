bc962165e60264102deddcaa5ea0b5ac908f6a92
Aitor Oses <aitor.oses@gmail.com>
2014-09-03 21:58:50 +0200

    refactor

--- transposer.js
+++ transposer.js
@@ -2,29 +2,29 @@ var transposer = require('transposer')
 
 // json payload
 var json = {
-	'soap:env': {
-		'soap:head': {
-			'soap:faulto': "some value"
-		},
-		'soap:body': {
-			'runtimedata':{
-				'tickets': [{
-						"id": 1,
-						"artist": 'beyonce',
-						"price": 70
-					},{
-						"id": 2,
-						"artist": 'bisbal',
-						"price": 20
-					},{
-						"id": 3,
-						"artist": 'bonjovi',
-						"price": 10
-					}
-				]
-			}
-		}
-	}
+    'soap:env': {
+        'soap:head': {
+            'soap:faulto': "some value"
+        },
+        'soap:body': {
+            'runtimedata':{
+                'tickets': [{
+                        "id": 1,
+                        "artist": 'beyonce',
+                        "price": 70
+                    },{
+                        "id": 2,
+                        "artist": 'bisbal',
+                        "price": 20
+                    },{
+                        "id": 3,
+                        "artist": 'bonjovi',
+                        "price": 10
+                    }
+                ]
+            }
+        }
+    }
 }
 // Execute
 main();
@@ -33,25 +33,25 @@ main();
 
 function main() {
 
-	// get all artists
-	// result: [{"id":3,"artist":"bonjovi","price":10}]
-	// performance: 1ms
-	path('$..artist');
+  // get all artists
+  // result: ["beyonce","bisbal","bonjovi"]
+  // performance: 0ms
+  path('$..artist');
 
-	// get the cheap tickets
-	// result: [{"id":3,"artist":"bonjovi","price":10}]
-	// performance: 1ms
-	path('$..tickets[?(@.price<30)]..artist');
+  // get the cheap ticket artists
+  // result: ["bisbal","bonjovi"]
+  // performance: 1ms
+  path('$..tickets[?(@.price<30)]..artist');
 
-	// get bonjovi's price
-	// result: [{"id":3,"artist":"bonjovi","price":10}]
-	// performance: 0ms
-	path('$..tickets[?(@.artist=bonjovi)]..price');
+  // get bonjovi's price
+  // result: [10]
+  // performance: 0ms
+  path('$..tickets[?(@.artist=bonjovi)]..price');
 
-	// Jquery like syntax
-	// Bon Jovi's price is: number 10
-	var bonjoviPrice = $('..tickets[?(@.artist=bonjovi)]..price', json, 0);
-	log("Bon Jovi's price is:", typeof bonjoviPrice, bonjoviPrice);
+  // Jquery like syntax
+  // Bon Jovi's price is: number 10
+  var bonjoviPrice = $('..tickets[?(@.artist=bonjovi)]..price', json, 0);
+  log("Bon Jovi's price is:", typeof bonjoviPrice, bonjoviPrice);
 }
 
 //----------------------------------------------
@@ -61,35 +61,35 @@ function main() {
  */
 
 function log() {
-	console.log.apply(null,arguments);
+  console.log.apply(null,arguments);
 }
 
 function measure(fn) {
-	log()
-	var start = new Date;
-	log('result: %s', JSON.stringify(fn()));
-	log('performance: %sms' , new Date - start);
-	log()
+  log()
+  var start = new Date;
+  log('result: %s', JSON.stringify(fn()));
+  log('performance: %sms' , new Date - start);
+  log()
 }
 
 function path(expression) {
-	var selectorFn = transposer(expression);
-	measure(function() {
-		return selectorFn.evaluate(json);
-	});
+  var selectorFn = transposer(expression);
+  measure(function() {
+      return selectorFn.evaluate(json);
+  });
 }
 
 function $(selector, json, index) {
-	var selectorFn = transposer('$' + selector);
-	try {
-		var result = selectorFn.evaluate(json);
-		if (index != null) {
-			return result[index];
-		} 
-		return result;
-	} catch (e) {
-		return [];
-	}
+  var selectorFn = transposer('$' + selector);
+  try {
+    var result = selectorFn.evaluate(json);
+    if (index != null) {
+        return result[index];
+    } 
+    return result;
+  } catch (e) {
+    return [];
+  }
 }
 
 
