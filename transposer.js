7203cd32c4b736c999cb5338d4b2644b4c4c2195
Aitor Oses <aitor.oses@gmail.com>
2014-09-03 21:50:06 +0200

    refactor

--- transposer.js
+++ transposer.js
@@ -1,33 +1,4 @@
 var transposer = require('transposer')
-var log = console.log;
-
-var measure = function(fn) {
-	log()
-	var start = new Date;
-	log('result: %s', JSON.stringify(fn()));
-	log('performance: %sms' , new Date - start);
-	log()
-}
-
-var path = function(expression) {
-	var selectorFn = transposer(expression);
-	measure(function() {
-		return selectorFn.evaluate(json);
-	});
-}
-
-var $ = function(selector, json, index) {
-	var selectorFn = transposer('$' + selector);
-	try {
-		var result = selectorFn.evaluate(json);
-		if (index != null) {
-			return result[index];
-		} 
-		return result;
-	} catch (e) {
-		return [];
-	}
-}
 
 // json payload
 var json = {
@@ -55,26 +26,70 @@ var json = {
 		}
 	}
 }
+// Execute
+main();
 
-// get all artists
-// result: [{"id":3,"artist":"bonjovi","price":10}]
-// performance: 2ms
-path('$..artist');
+//----------------------------------------------
 
-// get the cheap tickets
-// result: [{"id":3,"artist":"bonjovi","price":10}]
-// performance: 2ms
-path('$..tickets[?(@.price<30)]..artist');
+function main() {
 
-// get bonjovi's price
-// result: [{"id":3,"artist":"bonjovi","price":10}]
-// performance: 2ms
-path('$..tickets[?(@.artist=bonjovi)]..price');
+	// get all artists
+	// result: [{"id":3,"artist":"bonjovi","price":10}]
+	// performance: 1ms
+	path('$..artist');
 
-// Jquery like syntax
-// Bon Jovi's price is: number 10
-var bonjoviPrice = $('..tickets[?(@.artist=bonjovi)]..price', json, 0);
-log("Bon Jovi's price is: " +  typeof bonjoviPrice + ' ' + bonjoviPrice);
+	// get the cheap tickets
+	// result: [{"id":3,"artist":"bonjovi","price":10}]
+	// performance: 1ms
+	path('$..tickets[?(@.price<30)]..artist');
+
+	// get bonjovi's price
+	// result: [{"id":3,"artist":"bonjovi","price":10}]
+	// performance: 0ms
+	path('$..tickets[?(@.artist=bonjovi)]..price');
+
+	// Jquery like syntax
+	// Bon Jovi's price is: number 10
+	var bonjoviPrice = $('..tickets[?(@.artist=bonjovi)]..price', json, 0);
+	log("Bon Jovi's price is: " +  typeof bonjoviPrice + ' ' + bonjoviPrice);
+}
+
+/**
+ * HELPERS
+ */
+
+function log() {
+	console.log.apply(null,arguments);
+}
+
+function measure(fn) {
+	log()
+	var start = new Date;
+	log('result: %s', JSON.stringify(fn()));
+	log('performance: %sms' , new Date - start);
+	log()
+}
+
+function path(expression) {
+	var selectorFn = transposer(expression);
+	measure(function() {
+		return selectorFn.evaluate(json);
+	});
+}
+
+function $(selector, json, index) {
+	var selectorFn = transposer('$' + selector);
+	try {
+		var result = selectorFn.evaluate(json);
+		if (index != null) {
+			return result[index];
+		} 
+		return result;
+	} catch (e) {
+		return [];
+	}
+}
+
 
 
 
